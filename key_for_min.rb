dbe54ca512bb8cfeea523a6dd7210b58e3884bd8
github username <github email address>
2018-12-24 02:59:28 -0500

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,24 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+
+def key_for_min_value(arghash)
+    compval = Float::INFINITY
+    if arghash.empty? == true
+        return nil
+    end
+    arghash.collect do |name, val|
+        arghash.collect do |name, val|
+            if val <= compval
+                compval = val
+            end
+        end
+        if val == compval
+            return name
+        end
+    end
+end
+
 #### Without Nested Loop ####
 # def key_for_min_value(arghash)
 #   compval = Float::INFINITY
@@ -18,26 +36,3 @@
 #     end
 #   end
 # end
-
-def key_for_min_value(arghash)
-  compval = Float::INFINITY
-  if arghash.empty? == true
-    return nil
-  end
-  arghash.collect do |name, val|
-    arghash.collect do |name, val|
-      if val <= compval
-        compval = val
-      end
-    end
-    if val == compval
-      return name
-    end
-  end
-end
-
-
-# hash = {:blake => 500, :ashley => 2, :adam => 1}
-# hash = {}
-# key_for_min_value(hash)
-# puts name_hash
